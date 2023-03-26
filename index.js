256541f3719dc217c954460701882306eabfbcc8
Tev-J <tj_delph@yahoo.com>
2023-03-26 17:23:24 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// code your solution here
+function saturdayFun(doing = "roller-skate") {
+  return `This Saturday, I want to ${doing}!`;
+}
+
+const mondayWork = function (doing = "go to the office") {
+  return `This Monday, I will ${doing}.`;
+};
+
+function wrapAdjective(flair = "*") {
+  const innerFunction = function (param = "special") {
+    return `You are ${flair}${param}${flair}!`;
+  };
+  return innerFunction;
+}
